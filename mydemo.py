595defbdeb4e35d29fa72466e3e2009fe9ccf433
clach04 <clach04@gmail.com>
2020-08-28 19:54:09 +0000

    Added most basic demo to compliment demo.py

---
+++ mydemo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# super simple discovery demo
+
+import discoverhue
+
+
+found = discoverhue.find_bridges()
+for bridge in found:
+    print('    Bridge ID {br} at {ip}'.format(br=bridge, ip=found[bridge]))
+
